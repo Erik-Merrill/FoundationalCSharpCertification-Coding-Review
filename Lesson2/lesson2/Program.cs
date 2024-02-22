35ec4275fd9051b7b5982e1cba1dbd939777b654
Erik-Merrill <erik.merrill.business@gmail.com>
2024-02-22 14:12:56 -0700

    lesson2 complete

---
+++ Lesson2/lesson2/Program.cs
@@ -0,0 +1,5 @@
+﻿string firstName = "Bob";
+int digit = 3;
+double anotherNumber = 34.4;
+
+Console.WriteLine("Hello, " + firstName + "! You have " + digit + " messages in your inbox. The temperature is " + anotherNumber + " celsius.");
