b4eaa5112212d5d4170c6f4dd906d72afd3e8a7a
Erik-Merrill <erik.merrill.business@gmail.com>
2024-02-28 16:28:32 -0700

    Lesson 2.3 complete

---
+++ Module2/2.3/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] orderIds = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+
+foreach (string orderID in orderIds){
+    if (orderID.StartsWith("B")){
+        Console.WriteLine(orderID);
+    }
+}
+
