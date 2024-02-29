2f7d45d553595be4151782b7bb29794f2f421a80
Erik-Merrill <erik.merrill.business@gmail.com>
2024-02-29 10:27:49 -0700

    Lesson 2.4 complete

---
+++ Module2/2.4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+This program takes a literal sentence, reverses it, 
+and counts the number of times the letter 'o' appears.
+*/
+
+string sentence = "The quick brown fox jumps over the lazy dog.";
+
+char[] sentenceArray = sentence.ToCharArray();
+
+Array.Reverse(sentenceArray);
+
+int letterCount = 0;
+foreach (char letter in sentenceArray)
+ {
+     if (letter == 'o') { letterCount++; }
+ }
+
+string rebuiltSentence = new String(sentenceArray);
+
+Console.WriteLine(rebuiltSentence);
+Console.WriteLine($"'o' appears {letterCount} times.");
