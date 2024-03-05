36a37f853079f2b10b5d97843ce236387ab48e1b
Erik-Merrill <erik.merrill.business@gmail.com>
2024-03-05 13:49:25 -0700

    lesson 3.1 complete

---
+++ Module3/3.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security;
+
+string permission = "";
+int level = 13;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else 
+    {
+        Console.WriteLine("Welcome, Admin user");
+    }
+} 
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact Admin for access.");
+    }
+    else 
+    {
+        Console.WriteLine("You do not have sufficient privileges.");    
+
+    }
+} 
+else
+{
+        Console.WriteLine("You do not have sufficient privileges.");    
+}
