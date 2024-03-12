d31298a62ee9f1d69c79eb2b2b381a9d2c8fa8ca
Erik-Merrill <erik.merrill.business@gmail.com>
2024-03-12 13:26:30 -0600

    Lesson 4.1 anad 4.2 complete

---
+++ Module4/4.1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Floating point types:");
+
+Console.WriteLine($"float   : {float.MinValue} to {float.MaxValue} (with ~6 - 9 digits of precision)");
+Console.WriteLine($"double  : {double.MinValue} to {double.MaxValue} with ~15 to 17 digits of precision");
+Console.WriteLine($"decimal : {decimal.MinValue} to {decimal.MaxValue} with ~28 to 29 digits of precision");
+//Console.WriteLine($"long    : {long.MinValue} to {long.MaxValue}");
+/*
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
+
+Console.WriteLine($"byte    : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"short   : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint    : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong   : {ulong.MinValue} to {ulong.MaxValue}");
+*/
---
+++ Module4/4.2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+string[] values = { "12.3" , "45" , "ABC" , "11" , "DEF" };
+decimal total = 0;
+string message = "";
+
+foreach (string value in values)
+{
+    decimal numericalStorage = 0;
+    if (decimal.TryParse(value, out numericalStorage))
+    {
+        total += numericalStorage;
+    }
+    else {
+        message += value;
+    }
+}
+
+Console.WriteLine($"message = {message}");
+Console.WriteLine($"total = {total}");
+*/
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+int result1 = value1 / (int)value2;
+decimal result2 = value2 / (decimal)value3;
+float result3 = value3 / value1; 
+
+Console.WriteLine($"Divide value 1 by value 2, display the result as an int: {result1}");
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+Console.WriteLine($"Divide value 3 by value 1, display the result as a float: {result3}");
